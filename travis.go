7668d5987d2ca4c6eda41a034a80aed0c86b541a
Felix Breidenstein <mail@felixbreidenstein.de>
2018-09-04 01:31:03 +0200

    Extract more information from post and parse into templates

--- travis.go
+++ travis.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"net/url"
@@ -14,10 +15,24 @@ type TravisModule struct {
 	defaultChannel string
 }
 
+type repository struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Owner string `json:"owner_name"`
+	URL   string `json:"URL"`
+}
+
 type payload struct {
-	ID       int    `json:"id"`
-	State    string `json:"state"`
-	Duration int    `json:"duration"`
+	ID            int `json:"id"`
+	Slug          string
+	State         string     `json:"state"`
+	StatusMessage string     `json:"status_message"`
+	Duration      int        `json:"duration"`
+	Branch        string     `json:"branch"`
+	CommitMessage string     `json:"message"`
+	Repository    repository `json:"repository"`
+	Author        string     `json:"author_name"`
+	Commiter      string     `json:"committer_name"`
 }
 
 func (m *TravisModule) init(c *viper.Viper) {
@@ -33,6 +48,26 @@ func (m TravisModule) getChannelList() []string {
 }
 
 func (m TravisModule) getHandler() http.HandlerFunc {
+	const startedString = "\x0315{{ .Author }}\x03 started a build for \x0312{{ .Slug }}\x03 with the commit '{{ .CommitMessage }}' on branch {{ .Branch }}"
+	const finishedString = "\x0315{{ .Author }}\x03's build for \x0312{{ .Slug }}\x03 finished after {{ .Duration }}sec and {{ .StatusMessage }}"
+
+	BuildStatus := map[string]string{
+		"Pending":       "is \x0306pending\x03",
+		"Passed":        "has \x0303passed\x03",
+		"Fixed":         "is \x0303fixed\x03",
+		"Broken":        "is \x0304broken\x03",
+		"Failed":        "has \x0304failed\x03",
+		"Still Failing": "ist \x0304still failing\x03",
+		"Canceled":      "was \x0304canceled\x03",
+		"Errored":       "has \x0313errored\x03",
+	}
+
+	startedTemplate, err := template.New("travis event").Parse(startedString)
+	finishedTemplate, err := template.New("travis event").Parse(finishedString)
+	if err != nil {
+		log.Fatalf("Failed to parse eventString template: %v", err)
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Got http event for /travis")
 		r.ParseForm()
@@ -48,6 +83,20 @@ func (m TravisModule) getHandler() http.HandlerFunc {
 			log.Println("Not valid json")
 			log.Fatal(err)
 		}
-		fmt.Printf("Build took %d seconds\n", p.Duration)
+
+		p.Slug = r.Header["Travis-Repo-Slug"][0]
+		p.StatusMessage = BuildStatus[p.StatusMessage]
+		var buf bytes.Buffer
+
+		if p.State == "started" {
+			startedTemplate.Execute(&buf, &p)
+		} else {
+			finishedTemplate.Execute(&buf, &p)
+		}
+
+		messageChannel <- IRCMessage{
+			Messages: []string{buf.String()},
+			Channel:  m.defaultChannel,
+		}
 	}
 }
